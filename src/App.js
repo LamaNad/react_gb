809bf969cc9c05376b4c7ac4fcea2c866ce63303
nadjazahharovaee <alienlamahope@gmail.com>
2022-04-04 12:19:38 +0300

    Fix | Added Home screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./components/Home/Home";
-import { ChatList } from "./components/ChatList/ChatList";
+import { Home } from "./screens/Home/Home";
+import { Chat } from "./screens/Chat/Chat";
 
 function App() {
 
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/chat" element={<ChatList />}>
+        <Route path="/chat" element={<Chat />}>
         </Route>
         <Route path="*" element={<h4>404</h4>} />
       </Routes>
--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,5 +1,4 @@
-// import { Link } from "react-router-dom";
-import { MainLayout } from "../Layout/MainLayout";
+import { MainLayout } from '../../components/Layout/MainLayout';
 
 export const Home = () => {
     return (
