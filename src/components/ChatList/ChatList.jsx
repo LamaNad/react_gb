5ca036aaf6a3fcdc4a5f1e2cd9f2a4ebeae7dc0e
nadjazahharovaee <alienlamahope@gmail.com>
2022-04-05 16:43:46 +0300

    Added ChatList in a separate component #2

--- src/components/ChatList/ChatList.jsx
+++ src/components/ChatList/ChatList.jsx
@@ -1,24 +1,30 @@
 import { NavLink } from 'react-router-dom';
-import { Avatar, Grid, Paper, Typography } from '@mui/material';
+import { Avatar, Button, Grid, Paper, Typography } from '@mui/material';
 
 export const ChatList = ({ chats }) => {
   return (
-    <div className="content_chat-list">
-    {chats.map((chat) => (
-      <NavLink className={({ isActive }) => 'menu-link' + (isActive ? ' is-active' : '')} to={`/chat/${chat.id}`} key={chat.id}>
-        <Paper className="paper">
-          <Grid container wrap="nowrap" spacing={2}>
-            <Grid item>
-              <Avatar className='sender_avatar'>{chat.author.charAt(0)}</Avatar>
+    <div>
+      <Button onClick={() => {}
+          //setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'))
+        }>
+        Click
+      </Button>
+
+      {chats.map((chat) => (
+        <NavLink className={({ isActive }) => 'menu-link' + (isActive ? ' is-active' : '')} to={`/chat/${chat.id}`} key={chat.id}>
+          <Paper className="paper">
+            <Grid container wrap="nowrap" spacing={2}>
+              <Grid item>
+                <Avatar className='sender_avatar'>{chat.author.charAt(0)}</Avatar>
+              </Grid>
+              <Grid item xs zeroMinWidth className='chat_message'>
+                <div className='chats_author'>{chat.author}</div>
+                <Typography noWrap>{chat.data}</Typography>
+              </Grid>
             </Grid>
-            <Grid item xs zeroMinWidth className='chat_message'>
-              <div className='chats_author'>{chat.author}</div>
-              <Typography noWrap>{chat.data}</Typography>
-            </Grid>
-          </Grid>
-        </Paper>
-      </NavLink>
-    ))}
-  </div>
+          </Paper>
+        </NavLink>
+      ))}
+    </div>
   );
 }
