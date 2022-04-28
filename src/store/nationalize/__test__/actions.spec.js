ca3831513fd1c0b06e06ba583527c255ef81739a
nadjazahharovaee <alienlamahope@gmail.com>
2022-04-28 18:37:12 +0300

    install jest-fetch-mock | getNationalize Test

--- src/store/nationalize/__test__/actions.spec.js
+++ src/store/nationalize/__test__/actions.spec.js
@@ -1,7 +1,7 @@
-import { getNationalizeRequest, GET_NATIONALIZE_REQUEST } from "../action";
+import { getNationalize, getNationalizeRequest, getNationalizeSuccess, GET_NATIONALIZE_REQUEST } from "../action";
 
 describe('getNationalizeReq', () => {
-    it('return obj with predefined type', () => {
+    it('returns obj with predefined type', () => {
         const expected = {
             type: GET_NATIONALIZE_REQUEST,
         };
@@ -9,5 +9,25 @@ describe('getNationalizeReq', () => {
         const received = getNationalizeRequest();
 
         expect(received).toEqual(expected);
+    });
+});
+
+describe('getNationalize', () => {
+    it('dispatches getNationalizeReq', () => {
+        const mockDispatch = jest.fn();
+        fetch.mockResponse(JSON.stringify([]));
+
+        getNationalize("")(mockDispatch);
+
+        expect(mockDispatch).toHaveBeenCalledWith(getNationalizeRequest());
+    });
+
+    it('dispatches getNationalizeSuc with fetch result', async () => {
+        const data = [{name: "test"}];
+        fetch.mockResponse(JSON.stringify(data));
+        const mockDispatch = jest.fn();
+
+        await getNationalize("")(mockDispatch);
+        expect(mockDispatch).toHaveBeenLastCalledWith(getNationalizeSuccess(data));
     });
 });
